dd6bcdd0d7c6ecfe71bd1175965569b408b64e1c
Illy <Pinillya@gmail.com>
2018-03-31 15:36:54 +0200

    Borg Movement script

---
+++ Assets/borgMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class borgMovement : MonoBehaviour {
+	public float maxDrift = 50.0f;
+
+	private Vector3 targetPosition;
+	private int timer = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		this.targetPosition.x = 0;
+		this.targetPosition.y = 0;
+		this.targetPosition.z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.timer++;
+
+		float timeIntersecter = this.timer % 40;
+
+	
+		if (timeIntersecter == 0 ) {
+			this.targetPosition.x = Random.Range(-maxDrift, maxDrift);
+			this.targetPosition.y = Random.Range(-maxDrift, maxDrift);
+		}
+
+		float step = 4 * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+	}
+}
